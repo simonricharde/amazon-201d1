94adce3c8f106f663951adb1916f9d4138a3e9c4
Simon Richard Eswaramoorthy <seswaram@amazon.com>
2019-02-28 23:52:18 -0800

    Completed problem 5 by solving multiply array of numbers

--- class-05-git-branching-images-text/lab/app.js
+++ class-05-git-branching-images-text/lab/app.js
@@ -97,11 +97,17 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function multiplyArray(multArr) { //eslint-disable-line
+    var multiplication=1;
+    for(var i=0;i<multArr.length;i++) {
+        multiplication=(multiply(multArr[i],multiplication)[0]);
+    }
+    console.log(multiplication);
+    return ([multiplication, 'The numbers 2,3,4 have a product of 24.']);
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
